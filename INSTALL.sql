2bf848eb45309640cba708b9bead11e89426439c
tondol <tondol@gmail.com>
2014-10-23 17:41:01 +0900

    INSTALL.sqlを更新

--- INSTALL.sql
+++ INSTALL.sql
@@ -1,9 +1,11 @@
+--
+-- テーブルの構造 `lives`
+--
+
 CREATE TABLE IF NOT EXISTS `lives` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nicoLiveId` varchar(255) NOT NULL,
   `title` varchar(255) NOT NULL,
-  `filename` varchar(255) DEFAULT NULL,
-  `filesize` int(11) DEFAULT NULL,
   `retryCount` int(11) NOT NULL,
   `createdAt` datetime NOT NULL,
   `downloadedAt` datetime DEFAULT NULL,
@@ -12,6 +14,10 @@ CREATE TABLE IF NOT EXISTS `lives` (
   UNIQUE KEY `nicoLiveId` (`nicoLiveId`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
+--
+-- テーブルの構造 `logs`
+--
+
 CREATE TABLE IF NOT EXISTS `logs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `kind` varchar(255) NOT NULL,
@@ -19,4 +25,20 @@ CREATE TABLE IF NOT EXISTS `logs` (
   `message` text NOT NULL,
   `createdAt` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
+--
+-- テーブルの構造 `videos`
+--
+
+CREATE TABLE IF NOT EXISTS `videos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `liveId` int(11) NOT NULL,
+  `vpos` int(11) NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  `filesize` int(11) NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `modifiedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `liveId` (`liveId`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
